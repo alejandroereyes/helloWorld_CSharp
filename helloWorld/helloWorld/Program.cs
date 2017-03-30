2262a23889149bf5cd83243daeed249b1ef307a0
alex.reyes <alex.reyes@KC17DV03>
2017-03-30 16:28:07 -0500

    init commit,

---
+++ helloWorld/helloWorld/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string subject = "World";
+
+            if (args.Length > 0)
+            {
+
+                subject = args[0];
+            }
+
+            Console.WriteLine($"Hello {subject}!");
+        }
+    }
+}
